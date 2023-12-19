f7a1e987f58d8b2b120109fed495fc3b3bc09f84
maciopelo <maciej.k00h@gmail.com>
2023-12-19 14:52:05 +0100

    Handled basic implementation for news

---
+++ content/news/second-news.md
@@ -0,0 +1,6 @@
+---
+title: "Second news"
+date: "2023-12-11"
+---
+
+Second news with some contnet bla bla bla
--- pages/news.tsx
+++ pages/news.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import Layout from "@/components/Layout/Layout";
+import { getAllNewsIds } from "@/lib/utils";
 
-export default function News() {
+export default function News({ urls }) {
   return (
     <Layout pageTitle="Aktualności">
       <div>
@@ -10,4 +11,14 @@ export default function News() {
       </div>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const ids = getAllNewsIds();
+  const urls = ids.map((id) => `/news/${id}`);
+  return {
+    props: {
+      urls,
+    },
+  };
 }
