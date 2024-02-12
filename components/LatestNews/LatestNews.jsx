44b26e6c609c0baf36fb74019019799fafaedd61
Maciej Koch <45609899+maciopelo@users.noreply.github.com>
2024-02-12 14:13:45 +0100

    [Closes #9] Add global loader [LKS-9] (#23)

--- components/LatestNews/LatestNews.jsx
+++ components/LatestNews/LatestNews.jsx
@@ -1,37 +1,44 @@
-import styles from '@/components/LatestNews/LatestNews.module.scss'
-import 'aos/dist/aos.css'
-import Image from 'next/image'
-import article from '@/public/images/newsNew.png'
-import Link from 'next/link'
+import styles from '@/components/LatestNews/LatestNews.module.scss';
+import 'aos/dist/aos.css';
+import Image from 'next/image';
+import article from '@/public/images/newsNew.png';
+import Link from 'next/link';
 
 export default function LatestNews({ newsData }) {
-	const newsDataWithDates = newsData.map(data => ({
-		...data,
-		dateObject: new Date(data.date),
-	}))
+  const newsDataWithDates = newsData.map((data) => ({
+    ...data,
+    dateObject: new Date(data.date),
+  }));
 
-	const sortedNewDatas = newsDataWithDates.sort((a, b) => b.dateObject - a.dateObject)
+  const sortedNewDatas = newsDataWithDates.sort(
+    (a, b) => b.dateObject - a.dateObject
+  );
 
-	const latestNews = sortedNewDatas.slice(0, 3)
-	console.log(latestNews)
+  const latestNews = sortedNewDatas.slice(0, 3);
 
-	return (
-		<section className={styles.newsSection}>
-			<p className={styles.textNews}>najnowsze aktualności</p>
-			<div className={styles.containerNews}>
-				{latestNews.map(news => (
-					<div key={news.id} data-aos='flip-down' className={styles.news}>
-						<Link href={`/news/${news.id}`}>
-							<Image className={styles.newsImg} src={news.imageUrl} width={400} height={400} alt='first article' />
-							<div className={styles.newsShadow}></div>
-							<div className={styles.newsText}>
-								<p className={styles.newsHeading}>{news.title}</p>
-								<p className={styles.newsDate}>{news.date}</p>
-							</div>
-						</Link>
-					</div>
-				))}
-			</div>
-		</section>
-	)
+  return (
+    <section className={styles.newsSection}>
+      <p className={styles.textNews}>najnowsze aktualności</p>
+      <div className={styles.containerNews}>
+        {latestNews.map((news) => (
+          <div key={news.id} data-aos="flip-down" className={styles.news}>
+            <Link href={`/news/${news.id}`}>
+              <Image
+                className={styles.newsImg}
+                src={news.imageUrl}
+                width={400}
+                height={400}
+                alt="first article"
+              />
+              <div className={styles.newsShadow}></div>
+              <div className={styles.newsText}>
+                <p className={styles.newsHeading}>{news.title}</p>
+                <p className={styles.newsDate}>{news.date}</p>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
