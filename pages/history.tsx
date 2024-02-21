71d3c468615053407c84094842f782e7445b626c
Marek Mańka <74463586+Manioo77@users.noreply.github.com>
2024-02-21 18:58:43 +0100

    [Closes #24] Created history section - LKS-10 (#24)
    * created history section
    
    * updated history section after review
    
    * updated styles history section and added new sponsors

--- pages/history.tsx
+++ pages/history.tsx
@@ -1,15 +1,126 @@
-import Link from 'next/link'
-import Layout from '@/components/Layout/Layout'
+import { useState } from 'react';
+import Layout from '@/components/Layout/Layout';
+import styles from 'styles/history.module.scss';
+import { useMediaQuery } from 'react-responsive';
+
+const containerElements = [
+  {
+    heading: 'Ponad 60 lat',
+    paragraph:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia alias labore explicabo aliquam corporis. Quas explicabo amet, praesentium commodi minima dolorem provident molestiae voluptas aspernatur ad dolores ipsa eos. Nulla maxime quos modi omnis. Maiores distinctio placeat esse! In, facilis! Atque accusamus corporis placeat, dolorum eveniet iure sequi! Aspernatur neque perspiciatis accusantium deleniti, quasi facere. Debitis quas incidunt vel adipisci dicta eius iste at.',
+  },
+  {
+    heading: 'Osiągnięcia',
+    paragraph:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio mollitia dolor iste quos in facilis laboriosam animi repudiandae esse. Odit? Dinima dolorem provident molestiae voluptas aspernatur ad dolores',
+    list: [
+      '1 miejsce na śląsku w 2012r.',
+      '3 miejsce w powiecie w 1999r.',
+      '2 miejsce na śląsku w 1970r.',
+      '3 miejsce na śląsku w 1965r.',
+      '1 miejsce na śląsku w 1950r.',
+    ],
+  },
+  {
+    heading: 'Byli trenerze',
+    paragraph:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio mollitia dolor iste quos in facilis laboriosam animi repudiandae esse. Odit? Debitis quas incidunt vel adipisci dicta eius iste at.',
+    list: [
+      'Jan Nowak 2014-2019',
+      'Brayn Kozieł 2011-2014',
+      'Stanisław Lola 1990-2011',
+      'Marek Cezary 1970-1990',
+      'Adam Pon 1947-1970',
+    ],
+  },
+  {
+    heading: 'Byli zawodnicy',
+    paragraph:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio mollitia dolor iste quos in facilis laboriosam animi repudiandae esse. Odit?',
+    list: [
+      'Jan Nowak 2014-2019',
+      'Brayn Kozieł 2011-2014',
+      'Stanisław Lola 2003-2011',
+      'Bronisław Gnat 2000-2003',
+      'Maciej Wroński 1990-2000',
+      'Marek Cezary 1970-1990',
+      'Adam Pon 1947-1970',
+    ],
+  },
+];
 
 export default function History() {
-	return (
-		<Layout pageTitle='Historia'>
-			<section className='mainPage'>
-				<div>
-					<h1>Historia</h1>
-					<Link href='/'>go home</Link>
-				</div>
-			</section>
-		</Layout>
-	)
+  const [currentPage, setCurrentPage] = useState(0);
+  const isMobile = useMediaQuery({ maxWidth: 600 });
+
+  const handleNextPage = () => {
+    if (currentPage < containerElements.length - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  return (
+    <Layout pageTitle="Historia">
+      <section className="mainPage">
+        <div className={styles.wrapper}>
+          {isMobile ? (
+            <div className={styles.containerElement}>
+              <h1 className={styles.heading}>
+                {containerElements[currentPage].heading}
+              </h1>
+              <p className={styles.paragraph}>
+                {containerElements[currentPage].paragraph}
+              </p>
+              {containerElements[currentPage].list && (
+                <ul className={styles.trainersList}>
+                  {containerElements[currentPage].list.map((item, idx) => (
+                    <li key={idx}>{item}</li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          ) : (
+            containerElements.map((element, index) => (
+              <div key={index} className={styles.containerElement}>
+                <h1 className={styles.heading}>{element.heading}</h1>
+                <p className={styles.paragraph}>{element.paragraph}</p>
+                {element.list && (
+                  <ul className={styles.trainersList}>
+                    {element.list.map((item, idx) => (
+                      <li key={idx}>{item}</li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            ))
+          )}
+        </div>
+        {isMobile && (
+          <div className={styles.pagination}>
+            <button
+              className={styles.prevButton}
+              disabled={currentPage === 0}
+              onClick={handlePreviousPage}
+            >
+              Poprzednia
+            </button>
+
+            <button
+              className={styles.nextButton}
+              disabled={currentPage === containerElements.length - 1}
+              onClick={handleNextPage}
+            >
+              Następna
+            </button>
+          </div>
+        )}
+      </section>
+    </Layout>
+  );
 }
--- pages/news.tsx
+++ pages/news.tsx
@@ -1,99 +1,107 @@
-import Layout from '@/components/Layout/Layout'
-import Link from 'next/link'
-import { getAllNewsBasicData } from '@/lib/utils'
-import Image from 'next/image'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-import styles from 'styles/news.module.scss'
-import { useEffect, useState } from 'react'
-type NewsData = { date: string; id: string; title: string; imageUrl: string }
-type NewsProps = { newsData: NewsData[] }
+import Layout from '@/components/Layout/Layout';
+import Link from 'next/link';
+import { getAllNewsBasicData } from '@/lib/utils';
+import Image from 'next/image';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import styles from 'styles/news.module.scss';
+import { useEffect, useState } from 'react';
+type NewsData = { date: string; id: string; title: string; imageUrl: string };
+type NewsProps = { newsData: NewsData[] };
 
 export default function News({ newsData }: NewsProps) {
-	const [slidesToShow, setSlidesToShow] = useState(3)
+  const [slidesToShow, setSlidesToShow] = useState(3);
 
-	const newsDataWithDates = newsData.map(newData => ({
-		...newData,
-		dateObject: new Date(newData.date),
-	}))
+  const newsDataWithDates = newsData.map((newData) => ({
+    ...newData,
+    dateObject: new Date(newData.date),
+  }));
 
-	const sortedNewDatas = newsDataWithDates.sort((a: any, b: any) => b.dateObject - a.dateObject)
+  const sortedNewDatas = newsDataWithDates.sort(
+    (a: any, b: any) => b.dateObject - a.dateObject
+  );
 
-	useEffect(() => {
-		const handleResize = () => {
-			let newSlidesToShow = 3
-			let newSlidesToScroll = 3
+  useEffect(() => {
+    const handleResize = () => {
+      let newSlidesToShow = 3;
+      let newSlidesToScroll = 3;
 
-			if (window.innerWidth < 960) {
-				newSlidesToShow = 2
-				newSlidesToScroll = 2
-			}
+      if (window.innerWidth < 960) {
+        newSlidesToShow = 2;
+        newSlidesToScroll = 2;
+      }
 
-			if (window.innerWidth < 600) {
-				newSlidesToShow = 1
-				newSlidesToScroll = 1
-			}
+      if (window.innerWidth < 600) {
+        newSlidesToShow = 1;
+        newSlidesToScroll = 1;
+      }
 
-			setSlidesToShow(newSlidesToShow)
-			carouselSettings.slidesToScroll = newSlidesToScroll
-		}
+      setSlidesToShow(newSlidesToShow);
+      carouselSettings.slidesToScroll = newSlidesToScroll;
+    };
 
-		window.addEventListener('resize', handleResize)
+    window.addEventListener('resize', handleResize);
 
-		return () => {
-			window.removeEventListener('resize', handleResize)
-		}
-	}, [])
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
-	const carouselSettings = {
-		dots: true,
-		infinite: true,
-		speed: 500,
-		slidesToShow: slidesToShow,
-		slidesToScroll: slidesToShow,
-		arrows: false,
-	}
+  const carouselSettings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: slidesToShow,
+    slidesToScroll: slidesToShow,
+    arrows: false,
+  };
 
-	return (
-		<Layout pageTitle='Aktualności'>
-			<section className='mainPage'>
-				<div className={styles.wrapper}>
-					<div className={styles.containerMain}>
-						<Slider {...carouselSettings}>
-							{sortedNewDatas.map(sortedNewData => (
-								<div key={sortedNewData.id} data-aos='flip-down' className={styles.containerNews}>
-									<div className={styles.news}>
-										<Link href={`/news/${sortedNewData.id}`}>
-											<Image
-												className={styles.newsImg}
-												src={sortedNewData.imageUrl}
-												alt='first article'
-												width={400}
-												height={400}
-											/>
-											<div className={styles.newsShadow}></div>
-											<div className={styles.newsText}>
-												<p className={styles.newsHeading}>{sortedNewData.title}</p>
-												<p className={styles.newsDate}>{sortedNewData.date}</p>
-											</div>
-										</Link>
-									</div>
-								</div>
-							))}
-						</Slider>
-					</div>
-				</div>
-			</section>
-		</Layout>
-	)
+  return (
+    <Layout pageTitle="Aktualności">
+      <section className="mainPage">
+        <div className={styles.wrapper}>
+          <div className={styles.containerMain}>
+            <Slider {...carouselSettings}>
+              {sortedNewDatas.map((sortedNewData) => (
+                <div
+                  key={sortedNewData.id}
+                  data-aos="flip-down"
+                  className={styles.containerNews}
+                >
+                  <div className={styles.news}>
+                    <Link href={`/news/${sortedNewData.id}`}>
+                      <Image
+                        className={styles.newsImg}
+                        src={sortedNewData.imageUrl}
+                        alt="article"
+                        width={400}
+                        height={400}
+                      />
+                      <div className={styles.newsShadow}></div>
+                      <div className={styles.newsText}>
+                        <p className={styles.newsHeading}>
+                          {sortedNewData.title}
+                        </p>
+                        <p className={styles.newsDate}>{sortedNewData.date}</p>
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
 }
 
 export async function getStaticProps() {
-	const newsData = getAllNewsBasicData()
-	return {
-		props: {
-			newsData,
-		},
-	}
+  const newsData = getAllNewsBasicData();
+  return {
+    props: {
+      newsData,
+    },
+  };
 }
